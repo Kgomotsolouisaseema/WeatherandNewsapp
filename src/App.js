b6b93e956cdb625f9061afe0a497855abbc4316d
Kgomotso <kgomotsolouisaseema@gmail.com>
2023-08-28 10:39:51 +0200

    weatherandnewsappComplet

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Search from "./components/search/search";
 import Forecast from "./components/forecast/forecast";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import CurrentNews from "./components/current-news/CurrentNews";
+import NewList from "./components/current-news/NewList";
 
 import { useState } from "react";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
-  const [newsData, setNewsData] = useState([]);
+  // const [newsData, setNewsData] = useState([]);
+
+
   console.log(currentWeather);
 
   //function for current weather and forecast
@@ -23,8 +25,6 @@ function App() {
     const forecastFetch = fetch(
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
-    
-    // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
@@ -33,43 +33,16 @@ function App() {
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecast({ city: searchData.label, ...forecastResponse });
-
-        try {
-          const newsApiUrl = ` https://newsapi.org/v2/top-headlines?lat=${lat}&lon=${lon}&apiKey=018ab5e1d2924cf788f6b9303706e653`;
-          const response = await fetch(newsApiUrl);
-          if (!response.ok) {
-            throw new Error("Request failed");
-          }
-          const data = await response.json();
-          setNewsData(data.articles);
-          
-        } catch (error) {
-          console.log("Error fetching news data", error);
-        }
-
+    
       })
       .catch((err) => console.log("error fetching weather and forecast ", err));
   };
 
-  
-  
+ 
 
   console.log(currentWeather);
-  console.log(forecast?.list); //?
+  console.log(forecast?.list); 
 
-  // const fetchNews = async () => {
-  //   try {
-  //     const response = await fetch(newsApiUrl);
-  //     if (!response.ok) {
-  //       throw new Error("Request failed");
-  //     }
-  //     const data = await response.json();
-  //     setNewsData(data.articles);
-      
-  //   } catch (error) {
-  //     console.log("Error fetching news data", error);
-  //   }
-  // };
 
   //function to filter forecast data
   //  function filterMiddayTimestamps(forecast){
@@ -88,9 +61,7 @@ function App() {
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />} {/*data={middayTimestamps} */}
       <div className="flex-item">
-        {newsData.map((news, index) => (
-          <CurrentNews key={index} newsItem={news} className="news-card" />
-        ))}
+        <NewList/>
         {/* <button onClick={fetchNews}>FetchNews</button> */}
       </div>
     </div>
